4e9436d753a379d93eba423be97c0c3c16e185f1
gg47633 <gman121174@gmail.com>
2024-04-03 16:55:48 -0400

    Removed unnecessary comments

--- src/FileCreation.java
+++ src/FileCreation.java
@@ -14,7 +14,7 @@ public class FileCreation {
     String mediumReverseLocation = "src/ReverseMediumFiles/";
     String largeReverseLocation = "src/ReverseLargeFiles/";
 
-/**
+
     //Generate unsorted files
     for(int i = 1; i <= 30; i++) {
       fileGen.generate(10000, smallLocation + "smallFile" + i, 1);
@@ -26,7 +26,7 @@ public class FileCreation {
       fileGen.generate(10000, smallSortedLocation + "smallSorted", 2);
       fileGen.generate(100000, mediumSortedLocation + "mediumSorted", 2);
       fileGen.generate(1000000, largeSortedLocation + "largeSorted", 2);
-**/
+
     //Generate reverse-sorted files from previously generated sorted files
     fileGen.generate(10000, smallReverseLocation + "smallReverse", 3);
     fileGen.generate(100000, mediumReverseLocation + "mediumReverse", 3);
